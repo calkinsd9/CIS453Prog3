be72040a7a029a4204af3a6cce2ef5c47af77d95
David Calkins <calkinsd9@yahoo.com>
2017-11-22 16:08:46 -0500

    missed a change in Process

--- src/Process.java
+++ src/Process.java
@@ -24,7 +24,11 @@ public class Process {
         this.totalFaults = 0;
         this.totalReferences = 0;
         this.processID = processID;
-        this.pageTable = new HashMap<String,String>();
+        this.pageTable = new HashMap<String,String>();  //maps pages -> frames
+//        for (int i = 0; i < 64; i++)
+//        {
+//            this.pageTable.put(Integer.toString(i), "not in memory");
+//        }
     }
 
     public void pageReference(String pageNum)
@@ -32,7 +36,7 @@ public class Process {
         this.totalReferences++;
         String frame = this.pageTable.get(pageNum);
         if (frame == null)
-            this.pageTable.put(pageNum, "not in memory");            
+            this.pageTable.put(pageNum, "not in memory");
     }
     
     public void movedPageIntoMemory(String page, String frame)
